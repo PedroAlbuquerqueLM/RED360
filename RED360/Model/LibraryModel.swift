9eab5cb40a84f92e68ca43da15aba709e5b96883
PedroAlbuquerqueLM <pedro.albuquerque@acad.ifce.edu.br>
2018-11-23 00:30:20 -0300

    Fix Icons

---
+++ RED360/Model/LibraryModel.swift
@@ -0,0 +1,23 @@
+//
+//  LibraryModel.swift
+//  RED360
+//
+//  Created by Pedro Albuquerque on 22/11/18.
+//  Copyright © 2018 com.dimensiva.tecnologia.red360.app. All rights reserved.
+//
+
+import Foundation
+
+struct LibraryModel : Codable {
+    let id : Int?
+    let nome : String?
+    let urlCapa : String?
+    let urlArquivo : String?
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case nome = "nome"
+        case urlCapa = "urlCapa"
+        case urlArquivo = "urlArquivo"
+    }
+}
